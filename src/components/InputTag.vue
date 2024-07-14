b75d61c175f3a28f406c63d8497adbe8c8f4fda1
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-13 19:11:28 -0600

    feat(InputTag): implement the basic data and layout along the handleKeyDown function basic structure

--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -1,9 +1,30 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      currentValue: "",
+      tags: [],
+    };
+  },
+
+  methods: {
+    handleKeyDown(e) {
+      if (e.key === "Enter" && this.currentValue !== "") {
+        this.tags.push(this.currentValue);
+      }
+    },
+  },
+};
+</script>
 
 <template>
   <div class="inputTag">
-    <div class="tags"></div>
-    <input type="text" />
+    <div class="tags">
+      <div class="tag" v-for="(tag, index) in tags" :key="index">
+        {{ tag }}
+      </div>
+    </div>
+    <input type="text" v-model="currentValue" @keydown="handleKeyDown" />
   </div>
 </template>
 
